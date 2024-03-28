c4aba88e951f9ff55219cfe6423d61e14ffe8460
MansourBa <mvnoubenvtiv05@gmail.com>
2024-03-28 10:26:34 +0000

    28-03

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -10,6 +10,12 @@
   <meta name="_token" content="{{ csrf_token() }}">
   
   <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
+  
+  <!-- FontAwesome JS-->
+  <script defer src="{{asset('assets/plugins/fontawesome/js/all.min.js')}}"></script>
+    
+  <!-- App CSS -->  
+  <link id="theme-style" rel="stylesheet" href="{{asset('assets/css/portal.css')}}">
 
   <!-- plugin css -->
   {!! Html::style('assets/plugins/@mdi/font/css/materialdesignicons.min.css') !!}
